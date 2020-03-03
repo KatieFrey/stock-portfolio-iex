51cf0e0dd5a74ea8753fa07e0d7418b36c4debcf
Katharine Francis <katie-francis@hotmail.com>
2020-03-02 19:29:12 -0500

    Updates

---
+++ precache-manifest.0c1b4e3c7305c6b7d3f36251f3c7c2c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d55ee28058354e124d9ddbe94f876db3",
+    "url": "/stock-portfolio-iex/index.html"
+  },
+  {
+    "revision": "b8d7828edf7f55f916e6",
+    "url": "/stock-portfolio-iex/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "b8d7828edf7f55f916e6",
+    "url": "/stock-portfolio-iex/static/js/2.a5a9409c.chunk.js"
+  },
+  {
+    "revision": "5db2c778c72966adf8bbd2041fe9173f",
+    "url": "/stock-portfolio-iex/static/js/2.a5a9409c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cce8f108afd8ccebc0ce",
+    "url": "/stock-portfolio-iex/static/js/main.f0f2bd34.chunk.js"
+  },
+  {
+    "revision": "55f365fdacfc52ab0863",
+    "url": "/stock-portfolio-iex/static/js/runtime-main.24cd97fb.js"
+  }
+]);
